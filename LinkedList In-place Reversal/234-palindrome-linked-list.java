5346496f5ba650d6772789a9c1ccba98585cfe9a
milndpawar007 <milindpawar5555@gmail.com>
2026-04-23 12:39:29 -0500

    Add optimal palindrome linked list solution using fast/slow pointers and in-place reversal (O(n) time, O(1) space)

---
+++ LinkedList In-place Reversal/234-palindrome-linked-list.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        // Find middle
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Skip middle for odd length
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        // Reverse second half
+        slow = reverseList(slow);
+
+        ListNode firstHalf = head;
+
+        // Compare halves
+        while (slow != null) {
+            if (slow.val != firstHalf.val) {
+                return false;
+            }
+            slow = slow.next;
+            firstHalf = firstHalf.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
